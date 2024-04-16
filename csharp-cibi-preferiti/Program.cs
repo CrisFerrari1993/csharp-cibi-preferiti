54d48e16172a44d04a849cd1427cca4d6b12463b
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-04-16 16:35:53 +0200

    modifiche output

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -20,9 +20,11 @@ namespace csharp_cibi_preferiti
             Console.WriteLine(favouriteFoods.First());
             //- Il vostro cibo preferito ma non troppo (ultima posizione della classifica)
             Console.WriteLine(favouriteFoods.Last());
+
             //BONUS
             //Stampate a video anche il cibo di mezza classifica, cioè che si trova nella posizione mediana.Attenzione: gestire anche il caso se aveste una classifica con un numero di elementi pari. In questo caso vanno stampati i 2 elementi in centro alla vostra classifica.
-            // variabile che prende l'indice iniziale degli elementi nella metà
+
+            // variabile che prende l'indice degli elementi nella metà
             int startIndex = (favouriteFoods.Length - 3) / 3;
 
             // stampo gli elementi a schermo
