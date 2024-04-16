3a1124bd77636a4be23fb6c1375f52b288ac74e6
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-04-16 15:58:40 +0200

    Aggiungere i file di progetto.

---
+++ csharp-cibi-preferiti/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace csharp_cibi_preferiti
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] favouriteFoods = { "sushi", "pizza", "pasta al regu", "paella", "pizzoccheri", "cheesecake" };
+            Console.WriteLine(favouriteFoods.Length);
+            Array.Sort(favouriteFoods);
+            Console.WriteLine(favouriteFoods);
+        }
+    }
+}
