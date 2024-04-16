054ab6343106b9c416c51a601ffc291ba34c81a9
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-04-16 16:54:24 +0200

    fix finale

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -9,17 +9,17 @@ namespace csharp_cibi_preferiti
         {
             string[] favouriteFoods = { "sushi", "pizza", "pasta al regu", "paella", "pizzoccheri", "cheesecake" };
             //- La lunghezza della classifica
-            Console.WriteLine(favouriteFoods.Length);
+            Console.WriteLine($"L'array è lungo {favouriteFoods.Length}");
             //-La vostra classifica(dunque stampare l’intero array in ordine indicando la posizione in classifica)
             Array.Sort(favouriteFoods);
             for(int i = 0; i < favouriteFoods.Length; i++)
             {
-                Console.WriteLine($"Numero {i} : {favouriteFoods[i]}");
+                Console.WriteLine($"Numero {i + 1} : {favouriteFoods[i]}");
             }
             //- Il vostro cibo top (prima posizione della classifica)
-            Console.WriteLine(favouriteFoods.First());
+            Console.WriteLine($"Il mio piatto prefertito è {favouriteFoods.First()}");
             //- Il vostro cibo preferito ma non troppo (ultima posizione della classifica)
-            Console.WriteLine(favouriteFoods.Last());
+            Console.WriteLine($"Il mio piatto prefertito ma non troppo è {favouriteFoods.Last()}");
 
             //BONUS
             //Stampate a video anche il cibo di mezza classifica, cioè che si trova nella posizione mediana.Attenzione: gestire anche il caso se aveste una classifica con un numero di elementi pari. In questo caso vanno stampati i 2 elementi in centro alla vostra classifica.
@@ -27,10 +27,10 @@ namespace csharp_cibi_preferiti
             // variabile che prende l'indice degli elementi nella metà
             int startIndex = (favouriteFoods.Length - 3) / 3;
 
-            // stampo gli elementi a schermo
+            // stampo gli elementi a meta classifica a schermo
             for (int i = startIndex; i < startIndex + 2; i++)
             {
-                Console.WriteLine(favouriteFoods[i]);
+                Console.WriteLine($"Elemento a metà classifica: {favouriteFoods[i]}");
             }
         }
     }
